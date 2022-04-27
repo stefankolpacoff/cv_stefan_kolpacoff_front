c1d773511ddcafe0376ba5f128f2e5d7c17db5cf
stefankolpacoff <54561854+stefankolpacoff@users.noreply.github.com>
2022-04-27 13:48:36 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen text-2xl text-white bg-slate-500 text-shadow-md">
+      <p className="w-2/4 text-center">
+        Mon site créé par Vite a été déployé sur Vercel avec succès !
+      </p>
+    </div>
+  );
+}
+
+export default App;
